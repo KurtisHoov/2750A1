2f975edfc5061d1a69c743904f8e2334e2576119
Kurtis Hoover <66095179+KurtisHoov@users.noreply.github.com>
2022-01-23 21:51:55 -0500

    jan 23

--- SVGHelpers.c
+++ SVGHelpers.c
@@ -1,23 +1,70 @@
 #include "SVGParser.h"
 
 char* attributeToString(void* data){
-   
     Attribute* attribute = data;
     if (attribute == NULL) { return NULL; }
     char* output = malloc(strlen(attribute->name) + strlen(attribute->value) + 4);
     sprintf(output, "%s: %s\n", attribute->name, attribute->value);
-    
+
     //printf("Output = %s\n", output);
     return(output);
 }
-
 void deleteAttribute(void* data){
-    printf("we deleteing\n");
+
     Attribute *attribute = data;
+    printf("we deleteing %s\n", attribute->name);
+    free(attribute->name);
     free(attribute);
 }
-
 int compareAttributes(const void *first, const void *second){
     printf("we in compare\n");
+    return 3;
+}
+void deleteRectangle(void* data){
+    Rectangle *rect = data;
+    printf("we deleting rectangle %f\n", rect->y);
+    free(rect);
+
+}
+char* rectangleToString(void* data){
+    Rectangle* rect = data;
+    if (rect == NULL) { return NULL; }
+    char* output = malloc(50);
+    sprintf(output,"x = %.0f\ny = %.0f\nheight = %.0f\nwidth = %.0f\n",rect->x, rect->y, rect->width, rect->height);
+    return(output);
+
+}
+int compareRectangles(const void *first, const void *second){
+    return 3;
+}
+void deleteCircle(void* data){
+    Circle *circle = data;
+    printf("Deleting Circle\n");
+    free(circle);
+}
+char* circleToString(void* data){
+    Circle* circle = data;
+    if (circle == NULL) { return NULL; }
+    char* output = malloc(50);
+    sprintf(output,"cx = %.0f\ncy = %.0f\nr = %.0f\n",circle->cx, circle->cy, circle->r);
+    return(output);
+}
+int compareCircles(const void *first, const void *second){
+    return 3;
+}
+void deletePath(void* data){
+    Path * path = data;
+    printf("deleting path\n");
+    free(path);
+}
+char* pathToString(void* data){
+    Path * path = data;
+    if(path == NULL) { return NULL; }
+    printf("data :::::: %d\n", strlen(path->data));
+    char* output = malloc(strlen(path->data) + 9);
+    sprintf(output,"data = %s", path->data);
+    return (output);
+}
+int comparePaths(const void *first, const void *second){
     return 3;
 }
--- SVGParser.c
+++ SVGParser.c
@@ -1,75 +1,293 @@
 #include "SVGParser.h"
 #include "LinkedListAPI.c"
 #include "SVGHelpers.c"
+#include <string.h>
 
-static void print_element_names(xmlNode * a_node)
+char * SVGToString(const SVG* newSvg){
+    char *output = malloc(10000);
+    char *test = NULL;
+    sprintf(output, "");
+    printf("in SVG to String\n");
+    //for rectangles
+    if(newSvg->rectangles != NULL){
+        printf("rectangles\n");
+        strcat(output,"\nprinting rectangle:::::::::::::\n");
+        test = toString(newSvg->rectangles);
+        strcat(output, test);
+        free(test);
+        test = NULL;
+        Rectangle * newRect = newSvg->rectangles->head->data;
+        
+        if (newRect->otherAttributes != NULL) {
+            printf("otherAtt Rec\n");
+            strcat(output,"\nprinting rectangle attributes:::::::::\n");
+            test = toString(newRect->otherAttributes);
+            strcat(output, test);
+            free(test);
+            test = NULL;
+        }
+    }
+    //for circles
+    if(newSvg->circles != NULL){
+        printf("circles\n");
+        strcat(output,"\nprinting circle:::::::::::::\n");
+        test = toString(newSvg->circles);
+        //output = (char *)realloc(output, strlen(test) + 1);
+        strcat(output, test);
+        free(test);
+        test = NULL;
+
+        Circle * newCircle = newSvg->circles->head->data;
+        
+        if (newCircle->otherAttributes != NULL) {
+            strcat(output,"\nprinting circle attributes:::::::::\n");
+            test = toString(newCircle->otherAttributes);
+           // output = (char *)realloc(output, strlen(test) + 1);
+            strcat(output, test);
+            free(test);
+            test = NULL;
+        }
+    }
+    //for paths
+    if(newSvg->paths != NULL){
+        printf("paths\n");
+        strcat(output,"\nprinting path:::::::::::::\n");
+        test = toString(newSvg->paths);
+        printf("woenfoweifnoi %d\n", sizeof(test));
+        // output = (char *)realloc(output, strlen(test) + 1);
+        strcat(output, test);
+        free(test);
+        test = NULL;
+
+        Path * newPath = newSvg->paths->head->data;
+        
+        if (newPath->otherAttributes != NULL) {
+            strcat(output,"\nprinting path attributes:::::::::\n");
+            test = toString(newPath->otherAttributes);
+            // output = (char *)realloc(output, strlen(test) + 1);
+            strcat(output, test);
+            free(test);
+            test = NULL;
+        }
+    }
+    //for svg attributes
+    if (newSvg->otherAttributes != NULL) {
+        printf("svg\n");
+        strcat(output,"\nprinting svg attributes::::::::::\n");
+        test = toString(newSvg->otherAttributes);
+        //output = (char *)realloc(output, strlen(test) + 1 + sizeof(SVG));
+        strcat(output, test);
+        free(test);
+        test = NULL;
+    }
+    return(output);
+}
+void deleteSVG(SVG * newSvg){
+    if(newSvg->otherAttributes != NULL){
+        printf("freeing otherAtt\n");
+        freeList(newSvg->otherAttributes); //free svg attributes
+    }
+    if(newSvg->rectangles != NULL){
+        Rectangle *rect = newSvg->rectangles->head->data;
+        if(rect->otherAttributes != NULL){
+            printf("freeing rectAtts\n");
+            freeList(rect->otherAttributes); //free rect attributes
+        }
+        freeList(newSvg->rectangles); //free rect
+    }
+    if(newSvg->circles != NULL){
+        Circle *circle = newSvg->circles->head->data;
+        if(circle->otherAttributes != NULL){
+            printf("freeing circleAtts\n");
+            freeList(circle->otherAttributes); //free circle attributes
+        }
+        freeList(newSvg->circles); //free cirle
+    }
+    if(newSvg->paths != NULL){
+        Path *path = newSvg->paths->head->data;
+        if(path->otherAttributes != NULL){
+            printf("freeing pathAtts\n");
+            freeList(path->otherAttributes); //free circle attributes
+        }
+        freeList(newSvg->paths); //free cirle
+    }
+    
+    free(newSvg);
+}
+
+List * rectParse(xmlNode * a_node, SVG * newSvg){
+    xmlNode *cur_node = NULL;
+    cur_node = a_node;
+    Rectangle* newRect = malloc(sizeof(Rectangle));
+    newRect->otherAttributes = initializeList(&attributeToString, &deleteAttribute, &compareAttributes); //initalize rectangle attribute list
+
+    printf("going into rect loop\n");
+    xmlAttr *attr;
+    for(attr = cur_node->properties; attr != NULL; attr = attr->next){
+
+        xmlNode *value = attr->children;
+        char *attrName = (char *)attr->name;
+        char *cont = (char *)(value->content);
+
+        if(strcmp(attrName, "x") == 0){
+            newRect->x = atof(cont);
+            printf("\n x = %.0f", newRect->x);
+        }else if(strcmp(attrName, "y") == 0){
+            newRect->y = atof(cont);
+            printf("\n y = %.0f", newRect->y);
+        }else if(strcmp(attrName, "width") == 0){
+            newRect->width = atof(cont);
+            printf("\n width = %.0f", newRect->width);
+        }else if(strcmp(attrName, "height") == 0){
+            newRect->height = atof(cont);
+            printf("\n height = %.0f\n", newRect->height);
+            
+        }else{ //otherAttributes
+            Attribute* rectAtt = malloc(sizeof(Attribute) + strlen(cont) * sizeof(char) + 1);   //create new attribute node
+            rectAtt->name = malloc(sizeof(char) * strlen(attrName) + 1); //malloc name
+            sprintf(rectAtt->name,"%s", attrName); //set name
+            sprintf(rectAtt->value,"%s", cont); //set vlaue
+            printf("\trectAtt name: %s, value = %s\n", rectAtt->name, rectAtt->value);
+            insertBack(newRect->otherAttributes, rectAtt);
+        }
+    }
+    insertBack(newSvg->rectangles, newRect);
+}
+
+List * circleParse(xmlNode * a_node, SVG * newSvg){
+    xmlNode *cur_node = NULL;
+    cur_node = a_node;
+    Circle* newCircle = malloc(sizeof(Circle));
+    newCircle->otherAttributes = initializeList(&attributeToString, &deleteAttribute, &compareAttributes); //initalize circle attribute list
+
+    printf("going into circle loop\n");
+    xmlAttr *attr;
+    for(attr = cur_node->properties; attr != NULL; attr = attr->next){
+        xmlNode *value = attr->children;
+        char *attrName = (char *)attr->name;
+        char *cont = (char *)(value->content);
+
+        if(strcmp(attrName, "cx") == 0){
+            newCircle->cx = atof(cont);
+            printf("\n cx = %.0f", newCircle->cx);
+        }else if(strcmp(attrName, "cy") == 0){
+            newCircle->cy = atof(cont);
+            printf("\n cy = %.0f", newCircle->cy);
+        }else if(strcmp(attrName, "r") == 0){
+            newCircle->r = atof(cont);
+            printf("\n r = %.0f", newCircle->r);
+        }
+        else{ //otherAttributes
+            Attribute* circleAtt = malloc(sizeof(Attribute) + strlen(cont) * sizeof(char) + 1);   //create new attribute node
+            circleAtt->name = malloc(sizeof(char) * strlen(attrName) + 1); //malloc name
+            sprintf(circleAtt->name,"%s", attrName); //set name
+            sprintf(circleAtt->value,"%s", cont); //set vlaue
+            printf("\tcircleAtt name: %s, value = %s\n", circleAtt->name, circleAtt->value);
+            insertBack(newCircle->otherAttributes, circleAtt);
+        }
+    }
+    insertBack(newSvg->circles, newCircle);
+}
+List * pathParse(xmlNode * a_node, SVG * newSvg){
+    xmlNode *cur_node = NULL;
+    cur_node = a_node;
+    Path* newPath = malloc(sizeof(Path));
+    newPath->otherAttributes = initializeList(&attributeToString, &deleteAttribute, &compareAttributes); //initalize path attribute list
+
+    printf("going into path loop\n");
+    xmlAttr *attr;
+    for(attr = cur_node->properties; attr != NULL; attr = attr->next){
+        xmlNode *value = attr->children;
+        char *attrName = (char *)attr->name;
+        char *cont = (char *)(value->content);
+        if(strcmp(attrName, "d") == 0){
+            newPath = realloc(newPath, strlen(cont) + sizeof(Path) + 1);
+            sprintf(newPath->data, "%s", cont);
+            printf("here1\n");
+
+            printf("\n d = %s\n", newPath->data);
+        }
+        else{ //otherAttributes
+            printf("attrName = %s, cont = %s\n", attrName, cont);
+            Attribute* pathAtt = malloc(sizeof(Attribute) + strlen(cont) * sizeof(char) + 1);   //create new attribute node
+            printf("here2\n");
+
+            pathAtt->name = malloc(sizeof(char) * strlen(attrName) + 1); //malloc name
+            sprintf(pathAtt->name,"%s", attrName); //set name
+            sprintf(pathAtt->value,"%s", cont); //set vlaue
+            printf("\tpathAtt name: %s, value = %s\n", pathAtt->name, pathAtt->value);
+            insertBack(newPath->otherAttributes, pathAtt);
+        }
+    }
+    insertBack(newSvg->paths, newPath);
+}
+
+
+SVG * svgParse(xmlNode * a_node, SVG *newSvg)
 {
     xmlNode *cur_node = NULL;
+    //SVG* newSvg = malloc(sizeof(SVG)); //create node for new node type
     for (cur_node = a_node; cur_node != NULL; cur_node = cur_node->next) {
         //if the current node is and element
-       SVG* newSvg = malloc(sizeof(SVG)); //create node for new node type
-        if (cur_node->type == XML_ELEMENT_NODE) {
-            
-            sprintf(newSvg->namespace,"%s", cur_node->name); //assign name to nameSpace
-            //svg->namespace = cur_node->name;
-            //printf("node type: Element, name: %s\n", cur_node->name);
-            printf("node type: Element, name: %s\n", newSvg->namespace);
-            
-
-        }
        
-        
-        if(strcmp(newSvg->namespace, "title") == 0){ //if we are on the title the content will be found in the child
+        if (cur_node->type == XML_ELEMENT_NODE) {
+            //sprintf(newSvg->namespace,"%s", cur_node->name); //assign name to nameSpace
+            printf("node type: Element, name: %s\n", cur_node->name);
+            //printf("node type: Element, name: %s\n", newSvg->namespace);
+        }
+        if(strcmp(cur_node->name, "title") == 0){ //if we are on the title the content will be found in the child
             sprintf(newSvg->title,"%s", cur_node->children->content);
             printf("title = %s", newSvg->title);
         }
-            
         // Uncomment the code below if you want to see the content of every node.
         // if (cur_node->content != NULL){
         //     printf("  content: %s\n", cur_node->content);
         // }
-
-
         // Iterate through every attribute of the current node
         xmlAttr *attr;
-        newSvg->otherAttributes = initializeList(&attributeToString, &deleteAttribute, &compareAttributes); //initalize attribute list
+        printf("before entering the for loop - cur_node->name = %s\n", cur_node->name);
 
-        for (attr = cur_node->properties; attr != NULL; attr = attr->next)
-        { 
-                
+        if(strcmp(cur_node->name, "rect") == 0){
+            newSvg->rectangles = initializeList(&rectangleToString, &deleteRectangle, &compareRectangles); //initalize rectangle list
+            newSvg->rectangles = rectParse(cur_node, newSvg); //parse rectangle
+        }else if(strcmp(cur_node->name, "circle") == 0){
+            newSvg->circles = initializeList(&circleToString, &deleteCircle, &compareCircles); //initalize circle list
+            newSvg->circles = circleParse(cur_node, newSvg); //parse circle
+        }else if(strcmp(cur_node->name, "path") == 0){
+            newSvg->paths = initializeList(&pathToString, &deletePath, &comparePaths);
+            newSvg->paths = pathParse(cur_node, newSvg);
+        }else if(strcmp(cur_node->name, "svg") == 0){ 
+            newSvg->otherAttributes = initializeList(&attributeToString, &deleteAttribute, &compareAttributes); //initalize attribute list
+            newSvg->rectangles = NULL;
+            newSvg->circles = NULL;
+            newSvg->paths = NULL;
 
-            xmlNode *value = attr->children;
-            char *attrName = (char *)attr->name;
-            char *cont = (char *)(value->content);
-            Attribute* newAtt = malloc(sizeof(Attribute) + strlen(cont) * sizeof(char) + 1);   //create new attribute node
-            newAtt->name = malloc(sizeof(char) * strlen(attrName) + 1); //malloc name
-            sprintf(newAtt->name,"%s", attrName); //set name
-            sprintf(newAtt->value,"%s", cont); //set vlaue
-                
+            printf("going into svg attributes\n");
+            for (attr = cur_node->properties; attr != NULL; attr = attr->next){            
 
-           // printf("\tattribute name: %s, attribute value = %s\n", attrName, cont);
-            printf("\tattribute name: %s, attribute value = %s\n", newAtt->name, newAtt->value);
-            printf("node type: Element, name: %s\n", newSvg->namespace);
-
-            insertBack(newSvg->otherAttributes, newAtt);
-
-           // sprintf(newSvg->otherAttributes,"%s %s", newAtt->name, newAtt->value);
-           // printf("-----OA = %s\n", newSvg->otherAttributes);
-
-        }
-
-        if (newSvg->otherAttributes != NULL) {
-            printf("\n%s\n", toString(newSvg->otherAttributes));
+                xmlNode *value = attr->children;
+                char *attrName = (char *)attr->name;
+                char *cont = (char *)(value->content);
+           
+                Attribute* newAtt = malloc(sizeof(Attribute) + strlen(cont) * sizeof(char) + 1);   //create new attribute node
+                newAtt->name = malloc(sizeof(char) * strlen(attrName) + 1); //malloc name
+                sprintf(newAtt->name,"%s", attrName); //set name
+                sprintf(newAtt->value,"%s", cont); //set vlaue
+                printf("\tattribute name: %s, attribute value = %s\n", newAtt->name, newAtt->value);
+                insertBack(newSvg->otherAttributes, newAtt);
+            }
         }
         
-        print_element_names(cur_node->children);
-        printf("calling freeList()\n");
-        freeList(newSvg->otherAttributes);
+
+        svgParse(cur_node->children, newSvg);
+        
     }
-    
+    return(newSvg);
+
+
 }
- 
- 
+
+
  SVG* createSVG(const char* fileName){
     xmlDoc *doc = NULL;
     xmlNode *root_element = NULL;
@@ -83,8 +301,8 @@ static void print_element_names(xmlNode * a_node)
 
     /*Get the root element node */
     root_element = xmlDocGetRootElement(doc);
-
-    print_element_names(root_element);
+    SVG* newSvg = malloc(sizeof(SVG)); //create node for new node type
+    newSvg = svgParse(root_element, newSvg);
 
     /*free the document */
     xmlFreeDoc(doc);
@@ -94,8 +312,12 @@ static void print_element_names(xmlNode * a_node)
      *have been allocated by the parser.
      */
     xmlCleanupParser();
-
-     return 0;
+    if(newSvg != NULL){
+        printf("returning newSvg\n");
+        return newSvg;
+    }else{
+        return NULL;
+    }
 
  }
 
@@ -104,7 +326,13 @@ int main(int argc, char *argv[]){
         return(1);
 
 
-
-    createSVG(argv[1]);
-    
+    SVG * newSvg = createSVG(argv[1]);
+    if(newSvg != NULL){
+        printf("here!!!!!!!\n");
+        char * output = SVGToString(newSvg);
+        printf("-------------------------\n%s", output);
+        free(output);
+        printf("-------------------------\n");
+        deleteSVG(newSvg);
+    }
 }
